1a3d781a64932198b0c3305547a69215bde8a307
rob1gat <robin.gatelier@lilo.org>
2020-02-01 14:51:51 +0100

    Start throwing with player and add anim

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,9 +4,13 @@ extends RigidBody2D
 export (int) var id_player = 1
 export (float) var speed = 500
 var throw_strength: int = 0
+var has_crystal: bool = false
+var crystal
+var last_rot: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	connect("body_entered",self,"body_entered")
 	pass # Replace with function body.
 
 
@@ -25,14 +29,21 @@ func _process(delta):
 	
 	# Move player
 	linear_velocity = move_input * speed
+	rotation = last_rot
 	if(move_input != Vector2.ZERO):
 		rotation = move_input.angle() + PI/2
+		last_rot = rotation
 	
 	
-	if(is_loading_throw):
+	if(is_loading_throw and has_crystal):
 		throw_strength += 1
 	
-	if(is_releasing_throw):
-		# throw here
-		throw_strength = 0
+	if(is_releasing_throw and has_crystal):
+		throw(move_input)
 
+
+func throw(move_input:Vector2):
+	crystal.throw(throw_strength, move_input)
+
+func body_entered(body):
+	pass
