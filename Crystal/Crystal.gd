6ee9561b22d105b7e2d9260dfc22e02caf86630b
Elisa <elisa.ayala@etu.utc.fr>
2020-02-01 14:33:31 +0100

    Crystal done

--- Crystal/Crystal.gd
+++ Crystal/Crystal.gd
@@ -1,4 +1,5 @@
 extends RigidBody2D
+onready var utils = preload("res://Utils/Color.gd").new()
 
 enum CrystalState{
 	inactive,
@@ -6,11 +7,17 @@ enum CrystalState{
 }
 
 # Member variables here
+var _color
 var _state = CrystalState.inactive
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Initialize
 	setState(CrystalState.inactive)
+	
+	# Test
+	throw(800, Vector2(-2,2))
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,8 +30,9 @@ func take():
 	setState(CrystalState.active)
 	pass
 
+# Takes a force (float) and direction (Vector2, normalized or not)
 func throw(force, direction):
-	var impulse = force * direction
+	var impulse = force * direction.normalized()
 	apply_central_impulse(impulse)
 	pass
 
@@ -37,4 +45,12 @@ func setState(newState):
 		_state = CrystalState.inactive
 		# change sprite here
 
+# Takes a color from utils ColorEnum 
+func setColor(newColor):
+	# sets internal color value
+	_color = newColor
+	# get the matching color from the utils file and sets sprite color
+	$AnimatedSprite.modulate = utils.getColorValue(newColor)
 
+func isActive():
+	return _state == CrystalState.active;
