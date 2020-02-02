4ea373bc4327de0478a7200dcdb4634e59303785
Yoan <rockyoan@gmail.com>
2020-02-02 13:03:35 +0100

    crystal MIX COLOR OMG

--- Crystal/Crystal.gd
+++ Crystal/Crystal.gd
@@ -21,17 +21,46 @@ var _player = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("crystal")
+	connect("body_entered",self,"_onBodyEntered")
 	# Initialize
 	_setState(CrystalState.inactive)
 	pass # Replace with function body.
 
 # Called every frame
 func _process(_delta):
-	if _state == CrystalState.active and linear_velocity == Vector2.ZERO:
-		_setState(CrystalState.inactive)
 	if(_player):
 		linear_velocity = 30 * (_player.position - position + 40 * Vector2(cos(_player.last_rot), sin(_player.last_rot)))
 	pass
+
+func _onBodyEntered(body):
+	print("COLlISION")
+	if(body.is_in_group("crystal")):
+		var crystal = body
+		if(!isActive() or !crystal.isActive()):
+			return
+		var color2 = crystal.getColor()
+		var result = utils.getMixedColor(_color,color2)
+		
+		if(result == _color):
+			return
+		
+		setColor(result)
+		crystal.setColor(result)
+		
+		if(crystal._player == null && _player != null):
+			crystal.die()
+			return
+		if(crystal._player == null && _player == null):
+			if(utils.getMyDie(_color,color2)):
+				die()
+				return
+		if(crystal._player != null && _player == null):
+			die()
+			return
+		if(crystal._player != null && _player != null):
+			if(utils.getMyDie(_color,color2)):
+				die()
+		
 
 func take():
 	_setState(CrystalState.active)
@@ -68,8 +97,17 @@ func setColor(newColor):
 		$Sprite.set_texture(preload("res://Crystal/Assets/CrystalYellow.png"))
 	elif(_color == utils.ColorEnum.BLUE):
 		$Sprite.set_texture(preload("res://Crystal/Assets/CrystalBlue.png"))
+	elif(_color == utils.ColorEnum.ORANGE):
+		$Sprite.set_texture(preload("res://Crystal/Assets/CrystalOrange.png"))
+	elif(_color == utils.ColorEnum.PURPLE):
+		$Sprite.set_texture(preload("res://Crystal/Assets/CrystalPurple.png"))
+	elif(_color == utils.ColorEnum.GREEN):
+		$Sprite.set_texture(preload("res://Crystal/Assets/CrystalGreen.png"))
+		
 	# get the matching color from the utils file and sets sprite color
-
+func getColor():
+	return _color
+	
 func isActive():
 	return _state == CrystalState.active;
 
@@ -77,4 +115,7 @@ func follow(player):
 	_player = player
 	
 func die():
+	if(_player != null):
+		_player.release()
 	get_parent().remove_child(self)
+	pass
